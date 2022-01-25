97746b7d63b282994dc2f6114ec18c6d3c2d515c
Naélio Freires <naeliosistemas@gmail.com>
2022-01-25 16:49:33 -0300

    adding new expect

--- src/lib/calculator.js
+++ src/lib/calculator.js
@@ -1,3 +1,10 @@
 module.exports.sum = (num1, num2) => {
-  return num1 + num2;
+  const number01 = parseInt(num1, 10);
+  const number02 = parseInt(num2, 10);
+
+  if (Number.isNaN(number01) || Number.isNaN(number02)) {
+    throw new Error("Please check your input");
+  }
+
+  return number01 + number02;
 };
