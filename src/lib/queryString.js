c952504cae0d0b269a971388ef04ddc4bc48c740
Naélio Freires <naeliosistemas@gmail.com>
2022-02-03 16:14:13 -0300

    adding testes when there is an array to be converted

--- src/lib/queryString.js
+++ src/lib/queryString.js
@@ -1,7 +1,29 @@
-module.exports.queryString = obj => {
-  const srt = Object.entries(obj)
-    .map(item => `${item[0]}=${item[1]}`)
-    .join('&');
+function keyValueToString([key, value]) {
+  if (typeof value === 'object' && !Array.isArray(value)) {
+    throw new Error('Please check out params');
+  }
 
-  return srt;
-};
+  return `${key}=${value}`;
+}
+
+module.exports.queryString = obj =>
+  Object.entries(obj).map(keyValueToString).join('&');
+
+module.exports.parse = string =>
+  Object.fromEntries(
+    string.split('&').map(item => {
+      let [key, value] = item.split('=');
+
+      if (value.indexOf(',') > -1) {
+        value = value.split(',');
+      }
+
+      return [key, value];
+    }),
+  );
+
+/**
+ * indexOf()
+ *  - returns the first index at which a given element
+ * -  can be found in the array, or -1 if it is not present.
+ */
--- src/lib/queryString.spec.js
+++ src/lib/queryString.spec.js
@@ -1,4 +1,4 @@
-const { queryString } = require('./queryString');
+const { queryString, parse } = require('./queryString');
 
 describe('Object to query string', () => {
   it('should create a valid query string when an object is provided', () => {
@@ -8,5 +8,56 @@ describe('Object to query string', () => {
     };
 
     expect(queryString(object)).toBe('name=naelio&profession=developer');
+  });
+
+  it('should create a valid query string even when an array is passed as a value', () => {
+    const object = {
+      name: 'naelio',
+      abilities: ['JS', 'TDD'],
+    };
+
+    expect(queryString(object)).toBe('name=naelio&abilities=JS,TDD');
+  });
+
+  it('should throw an error when an object is passed as value', () => {
+    const object = {
+      name: 'naelio',
+      abilities: {
+        first: 'JS',
+        secondary: 'TDD',
+      },
+    };
+
+    expect(() => {
+      queryString(object);
+    }).toThrowError();
+  });
+});
+
+describe('Query string to object', () => {
+  it('should convert a queryString to object', () => {
+    const qs = 'name=naelio&profession=developer';
+
+    expect(parse(qs)).toEqual({
+      name: 'naelio',
+      profession: 'developer',
+    });
+  });
+
+  it('should convert a queryString of a single key-value', () => {
+    const qs = 'name=naelio';
+
+    expect(parse(qs)).toEqual({
+      name: 'naelio',
+    });
+  });
+
+  it('should convert a query string to an object taking care of comma separated values', () => {
+    const qs = 'name=Naelio&abilities=JS,TDD';
+
+    expect(parse(qs)).toEqual({
+      name: 'Naelio',
+      abilities: ['JS', 'TDD'],
+    });
   });
 });
